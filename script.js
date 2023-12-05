b423e1b83a1f050079048ca448c25f48e43f70c4
abhiavi7 <118717274+abhiavi7@users.noreply.github.com>
2023-12-05 20:14:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,4 @@
-// your code here
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Form with JS Manipulation</title>
-</head>
-<body>
-
-  <form id="myForm">
-    <label for="name">Name:</label>
-    <input type="text" id="name" name="name" required>
-
-    <br>
-
-    <label for="year">Year:</label>
-    <input type="number" id="year" name="year" required>
-
-    <br>
-
-    <input type="submit" id="button" value="Submit">
-  </form>
-
-  <h3 id="url">https://localhost:8080/</h3>
-
-  <script>
+<script>
     document.getElementById('myForm').addEventListener('submit', function(event) {
       event.preventDefault();
 
@@ -37,6 +11,3 @@
       urlElement.textContent = 'https://localhost:8080/?name=' + encodeURIComponent(name) + '&year=' + encodeURIComponent(year);
     });
   </script>
-
-</body>
-</html>
